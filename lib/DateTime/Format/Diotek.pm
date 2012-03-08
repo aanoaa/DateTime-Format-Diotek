a5f611d9bb15cdffe841036ff245f99106160d10
Hyungsuk Hong <aanoaa@gmail.com>
2012-03-09 00:18:31 +0900

    done

--- lib/DateTime/Format/Diotek.pm
+++ lib/DateTime/Format/Diotek.pm
@@ -1,16 +1,25 @@
 package DateTime::Format::Diotek;
-
-# ABSTRACT: DateTime::Format::Diotek
+# ABSTRACT: parse only YYYYMMDDhhmmss format
 use strict;
 use warnings;
+use DateTime::Format::Builder
+(
+    parsers => {
+        parse_datetime => [
+            {
+                params => [qw(year month day hour minute second)],
+                regex  => qr/^(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/,
+            }
+        ]
+    }
+);
 
 =head1 SYNOPSIS
 
-...
-
-=head1 DESCRIPTION
-
-...
+    use DateTime::Format::Diotek;
+    my $dt = DateTime::Format::Diotek->parse_datetime('20120203065530'); # YYYYMMDDhhmmss
+    print $dt->ymd; # 2012-02-03
+    print $dt->hms; # 06:55:30
 
 =cut
 
